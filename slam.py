9c23d3333d98780a9a5541befec1e82e00a6c732
Fran <kezicfran@gmail.com>
2023-08-12 21:49:12 +0200

    added descriptor matching

--- slam.py
+++ slam.py
@@ -3,34 +3,25 @@ import numpy as np
 
 
 class FeatureExtractor():
-    def __init__(self, img=None) -> None:
-        self.img = cv.resize(img, (1280, 720)
-                             ) if img is not None else None
-        self.orb = cv.ORB_create(4000)
+    def __init__(self):
+        self.orb = cv.ORB_create(3000)
+        self.bf = cv.BFMatcher()
+        self.last = None
 
-    def extract(self, img=None, maxCorners=4000, qualityLevel=0.01, minDistance=5):
-        if img is not None:
-            img = cv.resize(img, (1280, 720))
-            self.img = img
-        else:
-            img = self.img
+    def extract(self, img, maxCorners=3000, qualityLevel=0.01, minDistance=5):
         gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
         corners = cv.goodFeaturesToTrack(
             gray, maxCorners, qualityLevel, minDistance)
 
-        kps = []
-        for feat in corners:
-            kp = cv.KeyPoint(x=feat[0][0], y=feat[0][1], size=20)
-            kps.append(kp)
-        des = self.orb.compute(img, kps)
-        corners = np.intp(corners)
+        kps = [cv.KeyPoint(x=f[0][0], y=f[0][1], size=20) for f in corners]
+        kps, des = self.orb.compute(img, kps)
 
-        return kps, des
+        matches = None
+        if self.last is not None:
+            matches = self.bf.match(des, self.last['des'])
+        self.last = {'kps': kps, 'des': des}
 
-    def draw_corners(self, corners):
-        for i in corners:
-            u, v = i.ravel()
-            cv.circle(self.img, (u, v), 3, (0, 255, 0), -1)
+        return kps, des, matches
 
 
 def draw_kp(kp, img):
@@ -39,16 +30,20 @@ def draw_kp(kp, img):
         cv.circle(img, (u, v), 3, (0, 255, 0), -1)
 
 
+def draw_matches():
+    matches = sorted(matches, key=lambda x: x.distance)
+
+
 def process_frame(frame):
     frame = cv.resize(frame, (1280, 720))
-    kps, des = fe.extract(frame)
+    kps, des, matches = fe.extract(frame)
     draw_kp(kps, frame)
     cv.imshow('frame', frame)
     return kps, des
 
 
 if __name__ == "__main__":
-    cap = cv.VideoCapture('driving720slowed.mp4')
+    cap = cv.VideoCapture('driving720fast.mp4')
     fe = FeatureExtractor()
 
     while cap.isOpened():
