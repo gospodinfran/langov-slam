cf138a5e6c082ac3eb3b8e3699f4d48b51624bac
Fran <kezicfran@gmail.com>
2023-08-12 21:20:26 +0200

    small cleanup--separation of concern, good features to track, kps, des computing

--- slam.py
+++ slam.py
@@ -4,22 +4,47 @@ import numpy as np
 
 class FeatureExtractor():
     def __init__(self, img=None) -> None:
-        self.img = img
+        self.img = cv.resize(img, (1280, 720)
+                             ) if img is not None else None
+        self.orb = cv.ORB_create(4000)
 
-    def extract(self, img, maxCorners=3000, qualityLevel=0.01, minDistance=5):
-        self.img = img
+    def extract(self, img=None, maxCorners=4000, qualityLevel=0.01, minDistance=5):
+        if img is not None:
+            img = cv.resize(img, (1280, 720))
+            self.img = img
+        else:
+            img = self.img
         gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
         corners = cv.goodFeaturesToTrack(
             gray, maxCorners, qualityLevel, minDistance)
+
+        kps = []
+        for feat in corners:
+            kp = cv.KeyPoint(x=feat[0][0], y=feat[0][1], size=20)
+            kps.append(kp)
+        des = self.orb.compute(img, kps)
         corners = np.intp(corners)
 
-        return corners
+        return kps, des
 
-    def imshow(self, corners):
+    def draw_corners(self, corners):
         for i in corners:
-            x, y = i.ravel()
-            cv.circle(self.img, (x, y), 3, (0, 255, 0), -1)
-        cv.imshow('frame', self.img)
+            u, v = i.ravel()
+            cv.circle(self.img, (u, v), 3, (0, 255, 0), -1)
+
+
+def draw_kp(kp, img):
+    for p in kp:
+        u, v = map(lambda x: int(round(x)), p.pt)
+        cv.circle(img, (u, v), 3, (0, 255, 0), -1)
+
+
+def process_frame(frame):
+    frame = cv.resize(frame, (1280, 720))
+    kps, des = fe.extract(frame)
+    draw_kp(kps, frame)
+    cv.imshow('frame', frame)
+    return kps, des
 
 
 if __name__ == "__main__":
@@ -29,8 +54,7 @@ if __name__ == "__main__":
     while cap.isOpened():
         ret, frame = cap.read()
         if ret == True:
-            feats = fe.extract(frame)
-            fe.imshow(feats)
+            kps, des = process_frame(frame)
         else:
             break
         if cv.waitKey(1) == ord('q'):
